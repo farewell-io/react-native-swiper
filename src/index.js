7a2e9210b7de7f9f030744fee8e95cb10c9d1544
Rodney Carvalho <rodneyjcarvalho@gmail.com>
2017-11-28 14:00:47 -0800

    fix by Tennen

--- src/index.js
+++ src/index.js
@@ -256,9 +256,13 @@ export default class extends Component {
       ? height * props.index
       : width * props.index
 
+    if (state.total === initState.total && !props.updateIndex) {
+      initState.offset = this.internals.offset;
+    }
 
     this.internals = {
       ...this.internals,
+      offset: { ...initState.offset },
       isScrolling: false
     };
     return initState
